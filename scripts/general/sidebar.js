d50a323d2b6f76f369a35e5b15f7661e11d13cc2
Aryvg <abenettilahun6@gmail.com>
2026-07-25 19:11:47 +0100

    Update sidebar.js

--- scripts/general/sidebar.js
+++ scripts/general/sidebar.js
@@ -53,7 +53,7 @@ export const sidebarComponent = `
                 </div>
                 <div class="sidebar-text">Home</div>
             </a>
-            <a href="shorts.html" class="sidebarLink">
+            <a href="Shorts.html" class="sidebarLink">
                 <div>
                     <img src="images/images.jpeg" class="sidebar-image">
                 </div>
